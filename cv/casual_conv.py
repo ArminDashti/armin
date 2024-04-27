7a22c530ee90f6183a7dd555108ac277c65b4800
Armin <arminonline71@gmail.com>
2024-04-27 20:52:31 +0330

    Update casual_conv.py

--- cv/casual_conv.py
+++ cv/casual_conv.py
@@ -16,3 +16,26 @@ class Casual_Conv1D(nn.Module):
         
     def forward(self, x):
         return self.casual_conv(x)
+    
+#%%
+    
+from PIL import Image
+from torchvision import transforms as T
+import torch
+from torch import nn as nn
+import numpy as np
+import torchvision
+
+conv = nn.Conv2d(3, 6, kernel_size=3, stride=1, padding=1, dilation=2).to(torch.float64)
+relu = nn.Tanh()
+x = np.random.randint(0, 256, size=(1,3,200,200))
+tenb = x
+print(x.max())
+x = torch.tensor(x) / 256
+x = x.to(torch.float64)
+ten = x
+x = relu(conv(x))
+x.mul(256).int().max()
+ten.mul(256).int().max()
+#%%
+ten
