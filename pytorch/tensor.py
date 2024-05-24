22b16a4109f3348971e4277b9927279b217275b1
Armin <arminonline71@gmail.com>
2024-05-24 21:15:40 +0330

    Update tensor.py

--- pytorch/tensor.py
+++ pytorch/tensor.py
@@ -15,13 +15,22 @@ def numpy_to_tensor(tensor, device=None):
 
 
 def zeros(shape, device=None):
-    pass
+    zero_tensor = torch.zeros(*shape)
+    return zero_tensor
 
 
 def ones(shape, device=None):
-    pass
+    zero_tensor = torch.ones(*shape)
+    return zero_tensor
 
 
 def rand(shape, dtype='float', device=None):
-    pass
+    if dtype == 'int':
+        random_int_tensor = torch.randint(0, 10, shape)
+        return random_int_tensor
+    else:
+        random_float_tensor = torch.rand(*shape)
+        return random_int_tensor
+    
+    
 
