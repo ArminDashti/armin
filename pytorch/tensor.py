38da53ca915521939b6a883cc3fccc9aae144c7d
Armin <arminonline71@gmail.com>
2024-05-25 19:13:29 +0330

    Update tensor.py

--- pytorch/tensor.py
+++ pytorch/tensor.py
@@ -1,9 +1,9 @@
 import numpy as np
 import torch
-from armin_utils.pytorch.device import detect_device
+# from armin_utils.pytorch.device import detect_device
 
 
-default_device = detect_device()
+# default_device = detect_device()
 
 
 def to_numpy(tensor):
@@ -14,23 +14,32 @@ def numpy_to_tensor(tensor, device=None):
     pass
 
 
-def zeros(shape, device=None):
+def zeros(shape, grad=False, device=None):
     zero_tensor = torch.zeros(*shape)
     return zero_tensor
 
 
-def ones(shape, device=None):
+def ones(shape, grad=False, device=None):
     zero_tensor = torch.ones(*shape)
     return zero_tensor
 
 
-def rand(shape, dtype='float', device=None):
+def rand(shape, dtype='float', grad=False, device=None):
     if dtype == 'int':
-        random_int_tensor = torch.randint(0, 10, shape)
-        return random_int_tensor
+        rand_int = torch.randint(0, 10, shape)
+        return rand_int
     else:
-        random_float_tensor = torch.rand(*shape)
-        return random_int_tensor
-    
-    
+        rand_float = torch.rand(*shape)
+        return rand_float
 
+
+def fanin_init(tensor):
+    size = tensor.size()
+    if len(size) == 2:
+        fan_in = size[0]
+    elif len(size) > 2:
+        fan_in = np.prod(size[1:])
+    else:
+        raise Exception("Shape must be have dimension at least 2.")
+    bound = 1. / np.sqrt(fan_in)
+    return tensor.data.uniform_(-bound, bound)
